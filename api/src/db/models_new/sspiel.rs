cef3f6d2ee3981687c3c8d7a8fdaa84956b35739
John Doe <johndoe@example.com>
2023-05-17 17:36:20 +0200

    last fixes

--- api/src/db/models_new/sspiel.rs
+++ api/src/db/models_new/sspiel.rs
@@ -4,7 +4,6 @@ use crate::db::schema::*;
 use crate::utils::apikey;
 use serde::{Serialize, Deserialize};
 
-<<<<<<< HEAD
 use serde::{Serializer}; // 1.0.104
 
 fn apikey_serialize<S>(x: &i32, s: S) -> Result<S::Ok, S::Error>
@@ -14,20 +13,14 @@ where
     s.serialize_i32(apikey::generate())
 }
 
-=======
->>>>>>> 51d987b925a4c1f27126efdd48feb41281051aa8
 #[derive(Insertable, FromForm, Debug, Clone)]
 #[derive(Serialize, Deserialize)]
 #[diesel(table_name = sspiel)]
 pub struct NewSSpiel<'a> {
 	pub name: &'a str,
-<<<<<<< HEAD
 	pub url: &'a str,
 	//#[serde(default = "apikey::generate")]
 	#[serde(serialize_with = "apikey_serialize")]
-=======
-	#[serde(default = "apikey::generate")]
->>>>>>> 51d987b925a4c1f27126efdd48feb41281051aa8
 	pub apikeyid: i32,
 	//pub highscore: i32,
 	//pub best: i32
