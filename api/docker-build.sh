e8dd1aa5f06d2447db7982eff6e87dd24cfa8375
John Doe <johndoe@example.com>
2023-06-09 18:27:12 +0200

    docker build script ohne export

--- api/docker-build.sh
+++ api/docker-build.sh
@@ -4,10 +4,10 @@ docker image rm iic-debug
 #docker image rm iic-init
 docker image rm iic-release
 
-taskset -c 0,1,2,3 docker build -f Dockerfile.debug . -t iic-debug
+docker build -f Dockerfile.debug . -t iic-debug
 #docker build -f Dockerfile.init . -t iic-init
-taskset -c 0,1,2,3 docker build -f Dockerfile.release . -t iic-release
+docker build -f Dockerfile.release . -t iic-release
 
-docker save iic-debug:latest --output ../docker/iic-debug.tar
+#docker save iic-debug:latest --output ../docker/iic-debug.tar
 #docker save iic-init:latest --output ../docker/iic-init.tar
-docker save iic-release:latest --output ../docker/iic-release.tar
+#docker save iic-release:latest --output ../docker/iic-release.tar
